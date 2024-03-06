c23cd8bee5a16829c2d9f8da184657136e712ed9
Hakizimana <hshaka2004@gmail.com>
2024-03-06 20:33:02 +0300

    update5

---
+++ leap_year.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void check_leap(int b){
+    if((b%4==0 && b%100 !=0) || b%400 == 0){
+        printf("%d is Leap year\n",b);
+    }
+    else{
+        printf("%d is not leap year\n",b);
+    }
+}
+
+int main(){
+    int n = 2000;
+    //printf("Enter the year:");
+    //scanf("%d",&n);
+    for (int i = 0; i < 25; i++,n++)
+    {
+        check_leap(n);
+    }
+    return 0;
+}
